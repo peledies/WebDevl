bcb7f87ef8289fea88386e68fcf93aa5e71ff552
Deac <deac.karns@gmail.com>
2017-06-08 23:12:32 -0400

    begining flame parsing

--- .devl/etc/scripts/setup.sh
+++ .devl/etc/scripts/setup.sh
@@ -18,7 +18,23 @@ else
   project=$1
 fi
 
+provision() {
+  echo $1
+  parse_flame $1
+}
 
+parse_flame() {
+  flame=$(cat $SCRIPTPATH/../vagrant/provision_$1.flame)
+  grep -o '\bEVIL_\w*' $SCRIPTPATH/../vagrant/provision_$1.flame | while read line ; do
+    read -p "${cyan} Enter Value for ${line}: ${gold}" NICE </dev/tty
+    flame=$(sed "s/${line}/${NICE}/" <<< $flame)
+  done
+echo $flame
+  vagrant=$(cat $SCRIPTPATH/../vagrant/vagrant.evil)
+echo $vagrant
+  vagrant=$(sed "s/EVIL_PROVISION_APACHE/${flame}/" <<< $vagrant)
+  echo $vagrant
+}
 
 _menu () {
   echo "${green}  Provisioning Options"
@@ -38,15 +54,15 @@ _menu () {
   while true; do
     read -p "${cyan} Select an option from the list above: ${gold}" answer
     case $answer in
-      1 ) clear; provision_apache; _menu; break;;
-      2 ) clear; provision_php; _menu; break;;
-      3 ) clear; provision_composer; _menu; break;;
-      4 ) clear; provision_git_remote; _menu; break;;
-      5 ) clear; provision_mysql; _menu; break;;
-      6 ) clear; provision_nginx_rev_proxy; _menu; break;;
-      7 ) clear; provision_node; _menu; break;;
-      8 ) clear; provision_pm2; _menu; break;;
-      9 ) clear; provision_laravel; _menu; break;;
+      1 ) clear; provision "apache"; _menu; break;;
+      2 ) clear; provision "php"; _menu; break;;
+      3 ) clear; provision "composer"; _menu; break;;
+      4 ) clear; provision "git_remote"; _menu; break;;
+      5 ) clear; provision "mysql"; _menu; break;;
+      6 ) clear; provision "nginx_rev_proxy"; _menu; break;;
+      7 ) clear; provision "node"; _menu; break;;
+      8 ) clear; provision "pm2"; _menu; break;;
+      9 ) clear; provision "laravel"; _menu; break;;
       x ) clear; exit; break;;
 
       * ) echo "Please select a valid option.";;
