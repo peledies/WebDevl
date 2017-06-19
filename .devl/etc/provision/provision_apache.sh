58664867e6483ea5af40537a392d1d0ae25d51c6
Deac Karns <deac@sfp.net>
2017-06-19 14:42:27 -0400

    add new provisioners

--- .devl/etc/provision/provision_apache.sh
+++ .devl/etc/provision/provision_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME=${1:-"example.com"}
+NAME=${1:-"webdevl.com"}
 ADMIN=${2:-"JDoe@example.com"}
 ROOT=${3:-/var/www/html/}
 DIR=${4:-""} # OPTIONAL - Must not include pre slash, and must include trailing slash
@@ -8,6 +8,9 @@ DIR=${4:-""} # OPTIONAL - Must not include pre slash, and must include trailing 
 vagrant_build_log=/home/ubuntu/vm_build.log
 
 echo -e "\n=============================\n=== Provisioning Apache 2 ===\n=============================\n"
+
+echo -e "\n--- Updating packages list ---\n"
+apt-get -qq update
 
 echo -e "\n--- Installing [Apache 2] ---\n"
   apt install apache2 -y >> $vagrant_build_log 2>&1
@@ -29,7 +32,8 @@ echo -e "\n--- Disabling Apache module [mpm_event] ---\n"
 
 ## Create virtual host file for project
 echo -e "\n--- Creating Virualhost file for $NAME ---\n"
-cat <<EOF > /etc/apache2/sites-available/$NAME.conf
+touch /etc/apache2/sites-available/$NAME.conf
+cat <<EOF > /etc/apache2/sites-available/${NAME}.conf
 <VirtualHost *:80>
         ServerAdmin ${ADMIN}
         DocumentRoot ${ROOT}${DIR}
@@ -44,6 +48,12 @@ cat <<EOF > /etc/apache2/sites-available/$NAME.conf
 </VirtualHost>
 EOF
 
+echo -e "\n--- Enabling $NAME ---\n"
+  a2ensite $NAME >> $vagrant_build_log 2>&1
+
+echo -e "\n--- Disabling 000-default.conf ---\n"
+  a2dissite 000-default.conf >> $vagrant_build_log 2>&1
+
 ## Remove default web server directory tags from apache2.conf for security
 echo -e "\n--- Removing default web server directory from apache2.conf ---\n"
   sed -i '/^<Directory[ ]\/var\/www\/>/,/<\/Directory>/d' /etc/apache2/apache2.conf >> $vagrant_build_log 2>&1
@@ -52,5 +62,5 @@ echo -e "\n--- Removing /usr/share web server directory from apache2.conf ---\n"
   sed -i '/^<Directory[ ]\/usr\/share\/>/,/<\/Directory>/d' /etc/apache2/apache2.conf >> $vagrant_build_log 2>&1
 
 ## Restart apache
-echo -e "\n--- Restart Apache${default}"
+echo -e "\n--- Restart Apache --\n"
   service apache2 restart >> $vagrant_build_log 2>&1
