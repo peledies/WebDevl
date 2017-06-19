e9227124a3d118bd8b9558aa9222642e3603e1d8
Deac Karns <deac@sfp.net>
2017-06-19 16:29:44 -0400

    added definitve 5.6 installs

--- .devl/etc/provision/provision_php_56.sh
+++ .devl/etc/provision/provision_php_56.sh
@@ -14,4 +14,4 @@ echo -e "\n--- Removing PHP 7.0 ---\n"
   apt-get remove php7.0 --purge -y >> $build_log 2>&1
 
 echo -e "\n--- Installing Common PHP Server Dependencies ---\n"
-  apt install libapache2-mod-php php5.6 php-curl php-mysql php-mcrypt php-gd php-mbstring php-zip php5.6-simplexml -y >> $build_log 2>&1
+  apt install libapache2-mod-php5.6 php5.6 php5.6-curl php5.6-mysql php5.6-mcrypt php5.6-gd php5.6-mbstring php5.6-zip php5.6-simplexml -y
